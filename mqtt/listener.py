c88840c3da77af5620ca811c345fac0a27e3a278
MicIoT Team <miciot@micont.ru>
2021-05-21 21:18:35 +0000

    mqtt temp save to redis

--- mqtt/listener.py
+++ mqtt/listener.py
@@ -1,23 +1,30 @@
 import time
 
+import redis
 from paho.mqtt.client import Client
 
 import config
+
+rcache = redis.Redis()
 
 
 class LocalClient(Client):
     @staticmethod
     def on_message(client, userdata, msg):
-        print(msg.payload.decode())
+        msg = msg.payload.decode()
+        print(msg)
+        rcache.set('tmp', f'message {msg} {time.time()}')
 
     @staticmethod
     def on_connect(client, userdata, flags, rc):
         print('connected', time.time())
         client.subscribe('/')
+        rcache.set('tmp', f'connected and subscribed {time.time()}')
 
     @staticmethod
     def on_disconnect(client, userdata, rc):
         print('disconnected', time.time())
+        rcache.set('tmp', f'disconnected {time.time()}')
 
 
 client = LocalClient()
