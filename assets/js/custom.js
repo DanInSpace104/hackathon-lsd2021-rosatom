deb8d1fc70f34c5b4d4a85b4b1d2af8732406153
Shavkunov Konstantin <purposef@yandex.ru>
2021-05-22 05:35:26 +0500

    refactor(): add custom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,52 @@
 import {OrbitControls} from '/assets/js/OrbitControls.js';
+// import {readyPage} from "./wsocket-flask.js";
+
+import {} from '/assets/libjs/socket.io.min.js';
+// import {myfunc} from '/assets/js/custom.js';
+
+let namespace;
+let wsuripath;
+var connectStatus;
+function readyPage (myfunc) {
+
+    namespace = "/apisocket0"; //TODO в конфигах
+    wsuripath = "ws://2.57.186.96:5000/apisocket0"
+    console.log("connect"); //location.protocol + '//' + document.domain + ':' + location.port + namespace
+    const socket = io.connect(wsuripath);
+
+    socket.on('connect', function () {
+        console.log('Websocket connect', wsuripath);
+
+        socket.emit('join', 0); //TODO shemeid
+        // $("#serverState").text("Установлено");
+        // $("#serverState").removeClass('btn-red-full').addClass('btn-green-full');
+        connectStatus = 1;
+    });
+
+    socket.on('connection', function () {
+        console.log('Websocket connection');
+        $("#serverState").text("Устанавливается...");
+        $("#serverState").removeClass();
+        $("#serverState").addClass('btn-green-border');
+        connectStatus = 2;
+    });
+
+    socket.on('disconnect', function () {
+        console.log('Websocket disconnect');
+        $("#serverState").text("Разорвано");
+        $("#serverState").removeClass('btn-green-full').addClass('btn-red-full');
+        connectStatus = -1;
+    });
+
+
+    socket.on('my_response', myfunc);
+
+    socket.on('my_message', function (msg) { console.log(msg) })
+
+}
+
+
+
 
 function main() {
   const canvas = document.querySelector('#c');
@@ -14,7 +62,7 @@ function main() {
   const controls = new OrbitControls(camera, canvas);
   controls.target.set(0, 5, 0);
   controls.update();
-
+  var mesh;
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('black');
 
@@ -34,7 +82,7 @@ function main() {
       map: texture,
       side: THREE.DoubleSide,
     });
-    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh = new THREE.Mesh(planeGeo, planeMat);
     mesh.rotation.x = Math.PI * -.5;
     scene.add(mesh);
   }
@@ -47,14 +95,116 @@ function main() {
     scene.add(light);
   }
 
-  {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(5, 10, 2);
-    scene.add(light);
-    scene.add(light.target);
+    {
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('/assets/scene.gltf', (gltf) => {
+      const root = gltf.scene;
+      scene.add(root);
+
+      // compute the box that contains all the stuff
+      // from root and below
+      const box = new THREE.Box3().setFromObject(root);
+
+      const boxSize = box.getSize(new THREE.Vector3()).length();
+      const boxCenter = box.getCenter(new THREE.Vector3());
+
+      // set the camera to frame the box
+      frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+
+      // update the Trackball controls to handle the new size
+      controls.maxDistance = boxSize * 10;
+      controls.target.copy(boxCenter);
+      controls.update();
+    });
   }
+  function myfunc (msg) {
+    var i;
+    var text;
+    var lights;
+    lights = [{'x': 5, 'y': 10, 'z': 2}, {'x': 5, 'y': 5, 'z': 2}, {'x': 3, 'y': 2, 'z': 1}, {'x': 5, 'y': 1, 'z': 2},
+      {'x': 5, 'y': 5, 'z': 3}, {'x': 3, 'y': 15, 'z': 14}, {'x': 1750, 'y': 450, 'z': -2700}, {
+        'x': 1750,
+        'y': 450,
+        'z': -2700
+      }
+      , {'x': 1750, 'y': 450, 'z': -3270}, {'x': 1500, 'y': 450, 'z': -2700}, {'x': 2000, 'y': 450, 'z': -300}]
+
+    for (i = 0; i < lights.length; i++) {
+      // const color = 0xFF8844;
+      // const intensity = 0.1;
+      // const light = new THREE.AmbientLight(color, intensity);
+      // light.position.set(lights[i]['x'], lights[i]['y'], lights[i]['z']);
+      // scene.add(light);
+      // scene.add(light.target);
+      let bulbLight, bulbMat, hemiLight, stats;
+      let ballMat, cubeMat, floorMat;
+      const bulbGeometry = new THREE.SphereGeometry(200, 16, 8);
+      bulbLight = new THREE.PointLight(0xffee88, 1, 100, 2);
+
+      bulbMat = new THREE.MeshStandardMaterial({
+        emissive: 0xffffee,
+        emissiveIntensity: 1,
+        color: 0x000000
+      });
+
+      console.log(msg);
+      bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
+      bulbLight.position.set(lights[i]['x'], lights[i]['y'], lights[i]['z']);
+      bulbLight.castShadow = true;
+      bulbLight.decay = 100
+      bulbLight.power = 300
+      scene.add(bulbLight);
+    }
+  }
+  let hemiLight;
+  hemiLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 0.02 );
+  scene.add( hemiLight );
+
+
+
+//   var geometry = new THREE.CylinderBufferGeometry(200, 5, 20, 16, 4, true);
+// geometry.computeBoundingBox();
+// var material = new THREE.ShaderMaterial({
+//   uniforms: {
+//     color1: {
+//       value: new THREE.Color("red")
+//     },
+//     color2: {
+//       value: new THREE.Color("purple")
+//     },
+//     bboxMin: {
+//       value: geometry.boundingBox.min
+//     },
+//     bboxMax: {
+//       value: geometry.boundingBox.max
+//     }
+//   },
+//   vertexShader: `
+//     uniform vec3 bboxMin;
+//     uniform vec3 bboxMax;
+//
+//     varying vec2 vUv;
+//
+//     void main() {
+//       vUv.y = (position.y - bboxMin.y) / (bboxMax.y - bboxMin.y);
+//       gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+//     }
+//   `,
+//   fragmentShader: `
+//     uniform vec3 color1;
+//     uniform vec3 color2;
+//
+//     varying vec2 vUv;
+//
+//     void main() {
+//
+//       gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
+//     }
+//   `,
+//   wireframe: true
+// });
+//
+//   scene.add(new THREE.Mesh(geometry, material));
 
   function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
     const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
@@ -82,28 +232,7 @@ function main() {
     camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
   }
 
-  {
-    const gltfLoader = new GLTFLoader();
-    gltfLoader.load('/assets/scene.gltf', (gltf) => {
-      const root = gltf.scene;
-      scene.add(root);
 
-      // compute the box that contains all the stuff
-      // from root and below
-      const box = new THREE.Box3().setFromObject(root);
-
-      const boxSize = box.getSize(new THREE.Vector3()).length();
-      const boxCenter = box.getCenter(new THREE.Vector3());
-
-      // set the camera to frame the box
-      frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
-
-      // update the Trackball controls to handle the new size
-      controls.maxDistance = boxSize * 10;
-      controls.target.copy(boxCenter);
-      controls.update();
-    });
-  }
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
