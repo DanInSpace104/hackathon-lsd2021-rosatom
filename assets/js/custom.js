0776cebb5eb7f1481b26a64be4f3234d591ca434
Shavkunov Konstantin <purposef@yandex.ru>
2021-05-22 10:43:57 +0500

    add changing data from yaml

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,16 @@
 import {OrbitControls} from '/assets/libjs/OrbitControls.js';
 import {} from '/assets/libjs/socket.io.min.js';
+import {} from '/assets/libjs/yaml.min.js';
 
 let namespace;
 let wsuripath;
-var connectStatus;
-var myfunc;
-var data = 100;
+let connectStatus;
+var data = {};
 
-function readyPage () {
-    namespace = "/apisocket0"; //TODO в конфигах
+function readyPage() {
+    namespace = "/apisocket0";
     wsuripath = "ws://2.57.186.96:5000/apisocket0"
-    console.log("connect"); //location.protocol + '//' + document.domain + ':' + location.port + namespace
+    console.log("connect");
     const socket = io.connect(wsuripath);
     socket.on('connect', function () {
         console.log('Websocket connect', wsuripath);
@@ -28,239 +28,178 @@ function readyPage () {
         connectStatus = -1;
     });
 
-    socket.on('my_response', function (msg) { data = msg }  )
-    socket.on('my_message', function (msg) { console.log(msg) })
+    socket.on('my_response', function (msg) {
+        // data[msg.uuid] = msg
+        // console.log(data.length)
+        console.log(msg.uuid)
+    })
+    socket.on('my_message', function (msg) {
+        data[msg['uuid']] = msg
+    })
 }
 
 
-
-
 function main() {
-  readyPage();
-  const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
+    let schemeObject = YAML.load("/assets/scheme/sheme1.yaml")
+    console.log(schemeObject)
+    readyPage();
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
 
-  const fov = 45;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 100;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
+    const fov = 45;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
 
-  const controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 5, 0);
-  controls.update();
-  var mesh;
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color('black');
-
-  {
-    const planeSize = 40;
-
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.magFilter = THREE.NearestFilter;
-    const repeats = planeSize / 2;
-    texture.repeat.set(repeats, repeats);
-
-    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshPhongMaterial({
-      map: texture,
-      side: THREE.DoubleSide,
-    });
-    mesh = new THREE.Mesh(planeGeo, planeMat);
-    mesh.rotation.x = Math.PI * -.5;
-    scene.add(mesh);
-  }
-
-  {
-    const skyColor = 0xB1E1FF;  // light blue
-    const groundColor = 0xB97A20;  // brownish orange
-    const intensity = 1;
-    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-    scene.add(light);
-  }
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
+    var mesh;
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
 
     {
-    const gltfLoader = new GLTFLoader();
-    gltfLoader.load('/assets/scene.gltf', (gltf) => {
-      var root = gltf.scene;
-      root.transparent = true
-      scene.add(root);
+        const planeSize = 40;
 
-      // compute the box that contains all the stuff
-      // from root and below
-      const box = new THREE.Box3().setFromObject(root);
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.magFilter = THREE.NearestFilter;
+        const repeats = planeSize / 2;
+        texture.repeat.set(repeats, repeats);
 
-      const boxSize = box.getSize(new THREE.Vector3()).length();
-      const boxCenter = box.getCenter(new THREE.Vector3());
-
-      // set the camera to frame the box
-      frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
-
-      // update the Trackball controls to handle the new size
-      controls.maxDistance = boxSize * 10;
-      controls.target.copy(boxCenter);
-      controls.update();
-    });
-  }
-
-  var i;
-    var text;
-    var lights;
-    lights = [{'x': 5, 'y': 10, 'z': 2}, {'x': 5, 'y': 5, 'z': 2}, {'x': 3, 'y': 2, 'z': 1}, {'x': 5, 'y': 1, 'z': 2},
-      {'x': 5, 'y': 5, 'z': 3}, {'x': 3, 'y': 15, 'z': 14}, {'x': 1750, 'y': 450, 'z': -2700}, {
-        'x': 1750,
-        'y': 450,
-        'z': -2700
-      }
-      , {'x': 1750, 'y': 450, 'z': -3270}, {'x': 1500, 'y': 450, 'z': -2700}, {'x': 2000, 'y': 450, 'z': -300}]
-
-    for (i = 0; i < lights.length; i++) {
-      // const color = 0xFF8844;
-      // const intensity = 0.1;
-      // const light = new THREE.AmbientLight(color, intensity);
-      // light.position.set(lights[i]['x'], lights[i]['y'], lights[i]['z']);
-      // scene.add(light);
-      // scene.add(light.target);
-      let bulbLight, bulbMat, hemiLight, stats;
-      let ballMat, cubeMat, floorMat;
-      const color = new THREE.Color('rgb');
-      const bulbGeometry = new THREE.SphereGeometry(200, 16, 8);
-      bulbLight = new THREE.PointLight(0xf20000, 1, 100, 2);
-
-      bulbMat = new THREE.MeshStandardMaterial({
-        emissive: color,
-        emissiveIntensity: 1,
-        color: 0xffee88
-      });
-      bulbLight.name = "light" + i
-      bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
-      bulbLight.position.set(lights[i]['x'], lights[i]['y'], lights[i]['z']);
-      bulbLight.decay = 10
-      bulbLight.power = 3000
-      scene.add(bulbLight);
-
-    } let hemiLight;
-          hemiLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 0.02 );
-      scene.add( hemiLight );
-  const color = 0xf20000;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(1000, 1000, 0);
-    light.target.position.set(-5, 0, 0);
-    scene.add(light);
-    scene.add(light.target);
-
-
-
-
-//   var geometry = new THREE.CylinderBufferGeometry(200, 5, 20, 16, 4, true);
-// geometry.computeBoundingBox();
-// var material = new THREE.ShaderMaterial({
-//   uniforms: {
-//     color1: {
-//       value: new THREE.Color("red")
-//     },
-//     color2: {
-//       value: new THREE.Color("purple")
-//     },
-//     bboxMin: {
-//       value: geometry.boundingBox.min
-//     },
-//     bboxMax: {
-//       value: geometry.boundingBox.max
-//     }
-//   },
-//   vertexShader: `
-//     uniform vec3 bboxMin;
-//     uniform vec3 bboxMax;
-//
-//     varying vec2 vUv;
-//
-//     void main() {
-//       vUv.y = (position.y - bboxMin.y) / (bboxMax.y - bboxMin.y);
-//       gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-//     }
-//   `,
-//   fragmentShader: `
-//     uniform vec3 color1;
-//     uniform vec3 color2;
-//
-//     varying vec2 vUv;
-//
-//     void main() {
-//
-//       gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
-//     }
-//   `,
-//   wireframe: true
-// });
-//
-//   scene.add(new THREE.Mesh(geometry, material));
-
-  function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
-    const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
-    const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
-    const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
-    // compute a unit vector that points in the direction the camera is now
-    // in the xz plane from the center of the box
-    const direction = (new THREE.Vector3())
-        .subVectors(camera.position, boxCenter)
-        .multiply(new THREE.Vector3(1, 0, 1))
-        .normalize();
-
-    // move the camera to a position distance units way from the center
-    // in whatever direction the camera was from the center already
-    camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
-
-    // pick some near and far values for the frustum that
-    // will contain the box.
-    camera.near = boxSize / 100;
-    camera.far = boxSize * 100;
-
-    scene.getObjectByName('')
-    camera.updateProjectionMatrix();
-
-    // point the camera to look at the center of the box
-    camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
-  }
-
-
-
-  function resizeRendererToDisplaySize(renderer) {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-    if (needResize) {
-      renderer.setSize(width, height, false);
+        const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+        const planeMat = new THREE.MeshPhongMaterial({
+            map: texture,
+            side: THREE.DoubleSide,
+        });
+        mesh = new THREE.Mesh(planeGeo, planeMat);
+        mesh.rotation.x = Math.PI * -.5;
+        scene.add(mesh);
     }
-    return needResize;
-  }
 
-  function render() {
-    if (resizeRendererToDisplaySize(renderer)) {
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
+    {
+        const skyColor = 0xB1E1FF;  // light blue
+        const groundColor = 0xB97A20;  // brownish orange
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(light);
     }
-    // console.log(data)
-    // scene.getObjectByName('light1').position.set(data.val,100,200)
-    renderer.render(scene, camera);
-    // scene.getObjectByName('light2').material.color.set("rgb(255, 0, 0)");
+
+    {
+        const gltfLoader = new GLTFLoader();
+        gltfLoader.load('/assets/scene.gltf', (gltf) => {
+            var root = gltf.scene;
+            root.transparent = true
+            scene.add(root);
+
+            // compute the box that contains all the stuff
+            // from root and below
+            const box = new THREE.Box3().setFromObject(root);
+
+            const boxSize = box.getSize(new THREE.Vector3()).length();
+            const boxCenter = box.getCenter(new THREE.Vector3());
+
+            // set the camera to frame the box
+            frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+
+            // update the Trackball controls to handle the new size
+            controls.maxDistance = boxSize * 10;
+            controls.target.copy(boxCenter);
+            controls.update();
+        });
+    }
+
+    schemeObject = schemeObject['SensorParams']
+    for (let i = 0; i < schemeObject.length; i++) {
+
+        let bulbLight, bulbMat;
+
+        const bulbGeometry = new THREE.SphereGeometry(schemeObject[i]['size'][0] / 2, 16, 8);
+        bulbLight = new THREE.PointLight(0xffffff, 1, 100, 2);
+        bulbLight.name = schemeObject[i]['uuid']
+        let color = 0xffff00
+        if (bulbLight.name in data) {
+            color = new THREE.Color(0xf20000);
+        }
+        bulbMat = new THREE.MeshStandardMaterial({
+            emissive: color,
+            emissiveIntensity: 1,
+            color: 0xf20000
+        });
+
+        bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
+        bulbLight.position.set(schemeObject[i]['size'][0], schemeObject[i]['size'][1], schemeObject[i]['size'][2]);
+        bulbLight.decay = 10
+        bulbLight.power = 3000
+        scene.add(bulbLight);
+
+    }
+    let hemiLight;
+    hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.02);
+    scene.add(hemiLight);
+
+
+    function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
+        const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+        const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+        const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+        // compute a unit vector that points in the direction the camera is now
+        // in the xz plane from the center of the box
+        const direction = (new THREE.Vector3())
+            .subVectors(camera.position, boxCenter)
+            .multiply(new THREE.Vector3(1, 0, 1))
+            .normalize();
+
+        // move the camera to a position distance units way from the center
+        // in whatever direction the camera was from the center already
+        camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+
+        // pick some near and far values for the frustum that
+        // will contain the box.
+        camera.near = boxSize / 100;
+        camera.far = boxSize * 100;
+
+        scene.getObjectByName('')
+        camera.updateProjectionMatrix();
+
+        // point the camera to look at the center of the box
+        camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+    }
+
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render() {
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+        // console.log(data)
+        // scene.getObjectByName('light1').position.set(data.val,100,200)
+        renderer.render(scene, camera);
+        // scene.getObjectByName('light2').material.color.set("rgb(255, 0, 0)");
+        requestAnimationFrame(render);
+    }
+
     requestAnimationFrame(render);
-  }
-
-  requestAnimationFrame(render);
 }
 
 main();
 
-  myfunc = function(msg) {
-  var i;
-  console.log(msg);
-
-  }
+function myfunc(msg) {
+    console.log(msg);
+}
