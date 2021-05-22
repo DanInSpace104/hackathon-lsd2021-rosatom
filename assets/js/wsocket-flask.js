a7342af17921aa1bb4b06b0b71e054d74d4b5abb
Shavkunov Konstantin <purposef@yandex.ru>
2021-05-22 05:37:39 +0500

    refactor

--- assets/js/wsocket-flask.js
+++ assets/js/wsocket-flask.js
@@ -1,18 +1,22 @@
-var websocket = null;
-var schemepath = null;
+import {} from '/assets/libjs/socket.io.min.js';
+// import {myfunc} from '/assets/js/custom.js';
 
+let namespace;
+let wsuripath;
+var connectStatus;
+function readyPage (myfunc) {
 
-readyPage = function () {
-    template_name = "indexfield";
-    wsuripath = curConfig.attrs.socket_address; // wsUri + ":" + wsPort + namespace; //
-    console.log("connect to", wsuripath)
-    socket = io.connect(wsuripath);
+    namespace = "/apisocket0"; //TODO в конфигах
+    wsuripath = "ws://2.57.186.96:5000/apisocket0"
+    console.log("connect"); //location.protocol + '//' + document.domain + ':' + location.port + namespace
+    const socket = io.connect(wsuripath);
 
-    // Connect to websocket
     socket.on('connect', function () {
         console.log('Websocket connect', wsuripath);
-        $("#serverState").text("Установлено");
-        $("#serverState").removeClass('btn-red-full').addClass('btn-green-full');
+
+        socket.emit('join', 0); //TODO shemeid
+        // $("#serverState").text("Установлено");
+        // $("#serverState").removeClass('btn-red-full').addClass('btn-green-full');
         connectStatus = 1;
     });
 
@@ -21,7 +25,6 @@ readyPage = function () {
         $("#serverState").text("Устанавливается...");
         $("#serverState").removeClass();
         $("#serverState").addClass('btn-green-border');
-
         connectStatus = 2;
     });
 
@@ -32,84 +35,10 @@ readyPage = function () {
         connectStatus = -1;
     });
 
-    socket.on('my_response', function (msg) {
-        //$('#log').append('<br>' + $('<div>').text('Received #' + msg.count + ': ' + msg.data).html());
-        // console.log(msg);
-        // console.log("state="+connectStatus);
-        // if (msg["count"] > 1) {
-        //     if (template_name == "indexfield") processMessage(msg["data"]);
-        //     if (template_name == "controlview") processMessageControl(msg["data"]);
-        // };
-    });
 
-    var ping_pong_times = [];
-    var start_time;
-    window.setInterval(function () {
-        start_time = (new Date).getTime();
-        if (schemepath !== null) {
-            socket.emit('my_ping');
-        }
-        // console.log(connectStatus);
-    }, 5000);
+    socket.on('my_response', myfunc);
 
-
-    socket.on('my_pong', function () {
-        var latency = (new Date).getTime() - start_time;
-        // ping_pong_times.push(latency);
-        // ping_pong_times = ping_pong_times.slice(-30); // keep last 30 samples
-        // var sum = 0;
-        // for (var i = 0; i < ping_pong_times.length; i++)
-        //     sum += ping_pong_times[i];
-        // $('#ping-pong').text(Math.round(10 * sum / ping_pong_times.length) / 10);
-    });
-
-    $("#btnReset").click(function () {
-        if (socket != null) {
-            console.log("resetall");
-            socket.emit('resetall');
-        }
-        else
-            console.log("reset not work, socket not work");
-    });
+    socket.on('my_message', function (msg) { console.log(msg) })
 
 }
-
-function processMessage(message) {
-    /* Модифицировать, добавить проверку на тип ус-ва, тип индикатора...вообще переработать тут все*/
-    try {
-        var jso = JSON.parse(message);
-    }
-    catch (e) {
-        console.log("Ошибка json", message, e); // передать объект исключения обработчику ошибок
-    }
-    var s = " ";
-    var n = " ";
-    console.log("it's alive", jso["slaves"]);
-    // if (jso["slaves"] != null) { // message["slaves"]
-
-    //     for (ob of jso["slaves"]) { // message["slaves"]
-    //         {
-    //             // console.log(ob["name"]+ob["data"]);
-    //             //console.log();
-    //             let nameobj = ob["name"];
-    //             //TO-DO
-
-    //             for (bx of boxss) {
-    //                 //   console.log(bx.attrs.id);
-    //                 if (bx.attrs.id === nameobj) {
-    //                     //TO-DO
-    //                     // console.log("Yes baby",ob);
-    //                     let st = -1;
-    //                     if (ob["data"] == 0 || ob["data"] == 240) st = 1;
-    //                     if (ob["data"] > 0 && ob["data"] < 240) st = 2;
-
-    //                     if (ob["online"] == false) st = 0;
-    //                     bx.attrs.state = st;
-    //                     ChangeSate(bx);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    // stage.draw();
-}
+export {readyPage}
